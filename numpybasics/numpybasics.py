a4ae246455d7468707f0e181170d94c8ac519851
Selvaganapathi Sennan <34333910+Selvaganapathi06@users.noreply.github.com>
2019-08-02 08:50:33 +0530

    Add files via upload

---
+++ numpybasics/numpybasics.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+get_ipython().system('pip install numpy')
+
+
+# In[8]:
+
+
+get_ipython().system('pip install numpy --upgrade')
+
+
+# In[9]:
+
+
+#simple numpy
+import numpy as np
+
+aone =np.array([1,2,3,4,5,6])
+aone
+
+
+# In[12]:
+
+
+#Assign value to variable
+import numpy as np
+num = [1,2,3,4,5]
+aone =np.array(num)
+aone
+
+
+# In[17]:
+
+
+#Array of zeros
+import numpy as np
+array_of_zero = np.zeros((3,5))
+array_of_zero
+
