2ff10513f886202e93336f583bae5162e2e1f11f
Selvaganapathi Sennan <34333910+Selvaganapathi06@users.noreply.github.com>
2019-08-14 06:07:13 +0530

    Add files via upload

---
+++ arithmetic-operations/arithmetic_operations.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+#numpy arithmetic operation
+import numpy as np
+a = np.array([10,10,10,10])
+b = np.array([5,5,5,5])
+#Addition
+c= a+b
+print("Addition:")
+print(c)
+#subtraction
+d = a-b
+print("Subtraction:")
+print(d)
+#multiplications
+e = a*b
+print("multiplications:")
+print(e)
+#divisons
+f = a/b
+print("divisons:")
+print(f)
+#modulo divisons
+g = a%b
+print("modulo divisons:")
+print(g)
+#power
+h = a**3
+print("power:")
+print(h)
+
